827d5d32e54f260adb97c51d2de9497c0b6df25f
Cameron Raw <cameron.raw@codurance.com>
2024-06-05 14:02:37 +0200

    Adds error handling for dividing by zero

--- src/client.rs
+++ src/client.rs
@@ -11,10 +11,10 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let url = "http://[::1]:50051";
     let mut client = CalculatorClient::connect(url).await?;
 
-    let req = proto::CalculationRequest { a: 4, b: 5 };
+    let req = proto::CalculationRequest { a: 10, b: 0 };
     let request = tonic::Request::new(req);
 
-    let response = client.add(request).await?;
+    let response = client.divide(request).await?;
 
     println!("Response: {:?}", response.get_ref().result);
 
